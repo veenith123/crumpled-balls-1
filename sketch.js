5d61e35acb74c1cf64bdee43a4535a09babd7e6b
veenith123 <72656950+veenith123@users.noreply.github.com>
2020-11-20 19:30:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbin, ball, ground;
+var world;
+
+function setup(){
+	createCanvas(1350,550);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbin = new Dustbin(1200,510);
+	ball = new Paper(200,450,40);
+	ground = new Ground(width/2,530,width,20);
+	//Create a Ground
+
+	Engine.run(engine);
+}
+
+function draw(){
+
+	rectMode(CENTER);
+	background(0);
+
+	dustbin.display();
+	ball.display();
+	ground.display();
+
+}
+
+function keyPressed(){
+
+	if(keyCode === UP_ARROW){
+
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85});
+		
+	}
+	
+
+}
